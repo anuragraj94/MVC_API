2fad43e7ede991e4bb15d71ca96ace5b057f2d6a
Anurag <anuragraj94@gmail.com>
2021-03-23 16:42:55 +0530

    Token based authentication added

---
+++ web_Api/Controllers/LinqController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using web_Api.Models;
+
+namespace web_Api.Controllers
+{
+    public class LinqController : ApiController
+    {
+        DatabaseContext db = new DatabaseContext();
+        public IEnumerable<User> GetUsers()
+        {
+            IEnumerable<User> user = from lst in db.Users
+                                     select lst;
+            return user;
+        }
+
+        //api/user/2
+        public IEnumerable<User> GetUser(int id)
+        {            
+            IEnumerable<User> user = from lst in db.Users
+                        where lst.UserId == id
+                        select lst;
+            return user;
+        }
+
+        //api/user
+        [HttpPost]
+        public HttpResponseMessage AddUser(User model)
+        {
+            try
+            {
+                db.Users.Add(model);
+                db.SaveChanges();
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Created);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                return response;
+            }
+        }
+
+        //api/user
+        [HttpPut]
+        public HttpResponseMessage UpdateUser(User model, int id)
+        {
+            try
+            {
+                if (id == model.UserId)
+                {
+                    db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                    // db.Users.Add(model);
+                    db.SaveChanges();
+                    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                    return response;
+                }
+                else
+                {
+                    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.NotModified);
+                    return response;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                return response;
+            }
+        }
+
+        //api/user
+        public HttpResponseMessage DeleteUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user != null)
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                return response;
+            }
+            else
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.NotFound);
+                return response;
+            }
+        }
+    }
+}
---
+++ web_Api/Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace web_Api.Controllers
+{
+    public class TestController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/Test/forall")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Curren DateTime : " + DateTime.Now.ToString());
+        }
+
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/Test/Name")]
+        public IHttpActionResult Name()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok("Hello "+identity.Name);
+        }
+
+
+        [Authorize(Roles ="admin")]
+        [HttpGet]
+        [Route("api/Test/auth")]
+        public IHttpActionResult GetAdmin()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var roles = identity.Claims
+                                .Where(c => c.Type == ClaimTypes.Role)
+                                .Select(c => c.Value);
+            return Ok("Hello " + identity.Name +" Role: "+ string.Join(",", roles.ToList()));
+        }
+
+    }
+}
